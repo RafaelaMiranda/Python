0354446ff45a0e5b95634ab5a3064be80af3a281
Rafaela Miranda <rafsbrug@gmail.com>
2018-01-22 21:10:11 -0200

    Adição do exercicio 040

---
+++ Exercicios/ex040.py
@@ -0,0 +1,11 @@
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+media = (n1 + n2) / 2
+print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(n1, n2, media))
+
+if media < 5:
+    print('O aluno está REPROVADO.')
+elif media >= 5 and media < 6.9:
+    print('O aluno está em RECUPERAÇÃO.')
+else:
+    print('O aluno está APROVADO.')
